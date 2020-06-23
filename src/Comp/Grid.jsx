6466b9ab5a344e7b0cc3d4acfefda55dbd68fc13
Logan Van Hook <loganvan@att.net>
2020-06-23 11:39:45 -0400

    Remove grid hook

--- src/Comp/Grid.jsx
+++ src/Comp/Grid.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from "styled-components";
 
 export default class Grid extends React.Component
@@ -38,36 +38,6 @@ export default class Grid extends React.Component
     }
 }
 
-// function Grid(props)
-// {
-//     let CanvasRef = useRef(null);
-//     let Canvas = <GridContainer ref={CanvasRef} />;
-
-//     if (CanvasRef.current !== null)
-//     {
-//         var scale = window.devicePixelRatio;
-//         CanvasRef.current.width = Math.floor(props.size * scale);
-//         CanvasRef.current.height = Math.floor(props.size * scale);
-//         let Context = CanvasRef.current.getContext('2d');
-
-//         props.items.forEach((i)=>
-//         {
-//             if (i.state === 0)
-//                 Context.fillStyle = "rgba("+255+","+255+","+255+","+(255/255)+")";
-//             else
-//                 Context.fillStyle = "rgba("+0+","+0+","+0+","+(255/255)+")";
-
-//             Context.fillRect(i.posX, i.posY, 1, 1);
-//         });
-//     }
-
-//     return (
-//         Canvas
-//     );
-// }
-
 let GridContainer = styled.canvas`
     border: 3px solid black;
 `;
-
-//export default Grid;
